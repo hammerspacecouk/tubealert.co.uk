75dad40c7fb49ff5d5a7f05abb9693f44a877aba
David Marland <davidm@rland.me.uk>
2017-04-19 19:49:22 +0100

    Minify the code to see what works

--- webpack/client.js
+++ webpack/client.js
@@ -1,21 +1,21 @@
 'use strict';
 
 // setup react and routing
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/webapp/app.jsx';
-
-// other static assets (only relevant for client)
-import '../src/scss/all.scss';
-import '../src/imgs';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from '../src/webapp/app.jsx';
+//
+// // other static assets (only relevant for client)
+// import '../src/scss/all.scss';
+// import '../src/imgs';
 
 console.log('client.js loaded');
 
 // only modern browsers that support fetch will run this JS app
-if (window.fetch) {
-    console.log('mounting');
-    ReactDOM.render(<App />, document.getElementById('webapp'));
-}
+// if (window.fetch) {
+//     console.log('mounting');
+//     ReactDOM.render(<App />, document.getElementById('webapp'));
+// }
 
 // if ('serviceWorker' in window.navigator) {
 //     window.navigator.serviceWorker.register('/sw.js', {scope:'/'});
--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// Hack for Ubuntu on Windows: interface enumeration fails with EINVAL, so return empty.
-// try {
-//     require('os').networkInterfaces()
-// } catch (e) {
-//     require('os').networkInterfaces = () => ({})
-// }
-
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const path = require('path');
