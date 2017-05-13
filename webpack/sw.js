ce9f9d949580400558608545a1baca849222a25b
David Marland <davidm@rland.me.uk>
2017-05-13 19:38:43 +0100

    Service worker to use a different cached asset?

--- webpack/sw.js
+++ webpack/sw.js
@@ -22,8 +22,8 @@ self.addEventListener('install', event => event.waitUntil(
         .then(cache =>
             cache.addAll([
               '/',
-              STATIC_HOST + assetManifest['app.css'],
-              STATIC_HOST + assetManifest['app.js'],
+              STATIC_HOST + assetManifest['app.css'] + '?sw',
+              STATIC_HOST + assetManifest['app.js'] + '?sw',
             ])
         ).then(() => self.skipWaiting())
 ));
