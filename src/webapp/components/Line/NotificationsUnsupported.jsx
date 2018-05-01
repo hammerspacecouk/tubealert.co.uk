960fbc9f6e3ff923e0d7a37a9ab5fe86f2d3701c
David Marland <djmarland@users.noreply.github.com>
2018-05-01 19:46:39 +0100

    Update message with all the browsers

--- src/webapp/components/Line/NotificationsUnsupported.jsx
+++ src/webapp/components/Line/NotificationsUnsupported.jsx
@@ -8,8 +8,8 @@ const NotificationsUnsupported = () => (
       <strong>Your browser does not support notifications.</strong>
     </p>
     <p className="g-unit">
-                Push Notifications on the open web are currently only supported
-                by Firefox and Chrome. They are not supported on iOS, as the
+                Push Notifications on the open web are currently supported
+                by Edge, Chrome, Firefox, Opera & Samsung. They are not supported on iOS, as the
                 underlying browser is Safari, which does not support notifications.
             </p>
     <p className="g-unit">
